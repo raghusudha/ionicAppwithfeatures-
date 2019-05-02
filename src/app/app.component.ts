be01b017b80c0e0e0ae22739b851d517b442a6c9
raghusudha <sudharanibotlagunta@gmail.com>
2019-05-02 14:28:23 +0530

    notifications

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,32 +3,43 @@ import { Component } from '@angular/core';
 import { Platform } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
+import { FcmService} from '../app/fcm.service';
+import { ToastController } from '@ionic/angular';
 
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html'
 })
 export class AppComponent {
-  public appPages = [
-    {
-      title: 'Home',
-      url: '/home',
-      icon: 'home'
-    },
-    {
-      title: 'List',
-      url: '/list',
-      icon: 'list'
-    }
-  ];
-
   constructor(
     private platform: Platform,
     private splashScreen: SplashScreen,
-    private statusBar: StatusBar
+    private statusBar: StatusBar,
+    private fcm:FcmService,
+    private toastController:ToastController
   ) {
     this.initializeApp();
   }
+  private async presentToast(message) {
+    const toast = await this.toastController.create({
+    message,
+    duration: 3000
+    });
+    toast.present();
+    }
+    
+    private notificationSetup() {
+    this.fcm.getToken();
+    this.fcm.onNotifications().subscribe(
+    (msg) => {
+    if (this.platform.is('ios')) {
+    this.presentToast(msg.aps.alert);
+    } else {
+    this.presentToast(msg.body);
+    }
+    });
+    }
+    
 
   initializeApp() {
     this.platform.ready().then(() => {
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,10 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouteReuseStrategy } from '@angular/router';
+import { Firebase } from '@ionic-native/firebase/ngx';
+
+import { AngularFireModule } from '@angular/Fire';
+import { AngularFirestoreModule } from '@angular/Fire/firestore';
 
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
@@ -10,18 +14,31 @@ import { AppComponent } from './app.component';
 import { BarcodeScanner } from '@ionic-native/barcode-scanner/ngx';
 import { AppRoutingModule } from './app-routing.module';
 
+
+const config = {
+  apiKey: "AIzaSyAfPe3obfn5-uXSAIo04V7ZKLVUA3X2BcY",
+  authDomain: "pushnotifications-44f1b.firebaseapp.com",
+  databaseURL: "https://pushnotifications-44f1b.firebaseio.com",
+  projectId: "pushnotifications-44f1b",
+  storageBucket: "pushnotifications-44f1b.appspot.com",
+  messagingSenderId: "1021962089268"
+};
+
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
   imports: [
     BrowserModule,
     IonicModule.forRoot(),
-    AppRoutingModule
+    AppRoutingModule,
+    AngularFireModule.initializeApp(config), 
+    AngularFirestoreModule,
   ],
   providers: [
     StatusBar,
     SplashScreen,
     BarcodeScanner,
+    Firebase,
     { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }
   ],
   bootstrap: [AppComponent]
